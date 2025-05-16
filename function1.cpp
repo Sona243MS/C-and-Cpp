dfbe70c761caacdda658babcc53104abf88cd57a
Sona243MS <sona.simmy@s.amity.edu>
2025-05-16 21:24:09 +0530

    Added the code for function Overloading

--- function1.cpp
+++ function1.cpp
@@ -4,4 +4,18 @@ void sum(int a,int b)
 {
 	cout<<"Sum of two integers="<<a+b<<endl;
 }
+void sum(int a,b,c)
+{
+	cout<<"Sum of three integers(Function Overloading)="<<a+b+c<<endl;
+}
+int main()
+{
+	int a,b,c;
+	cout<<"Enter three integers=";
+	cin>>a>>b>>c;
+	sum(a,b);
+	sum(a,b,c);
+	return 0;
+}
+
 
