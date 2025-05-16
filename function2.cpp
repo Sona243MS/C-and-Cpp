76cbeb7b5451cb0e33531651662a06dac5f2ecbf
Sona243MS <sona.simmy@s.amity.edu>
2025-05-16 21:28:50 +0530

    Added the function overloading code for three member functions

---
+++ function2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void area(int r)
+{
+	cout<<"Area of circle="<<3.14*r*r<<endl;
+}
+void area(int len, int br)
+{
+	cout<<"Area of rectangle="<<len*br<<endl;
+}
+void area(int b,int h)
+{
+	cout<<"Area of a triangle="<<0.5*b*h<<endl;
+}
+
