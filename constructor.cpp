dc09db896f1a46ff8ff80b47bb93c7d514dbe585
Sona243MS <sona.simmy@s.amity.edu>
2025-05-18 18:31:24 +0530

    Added the the function to check the greatest of two numbers

---
+++ constructor.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+class studentt
+{
+	char name[10];
+	int rno;
+	double fee;
+	public:
+
---
+++ function3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void maximum(int a,int b)
+{
+	int gr=(a>b)?a:b;
+	cout<<gr<<"is maximum";
+}
+
