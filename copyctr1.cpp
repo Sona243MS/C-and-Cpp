4fd41f3b58690c57a4bf471b0432acaa1e598ef2
Sona243MS <sona.simmy@s.amity.edu>
2025-05-23 06:00:23 +0530

    Added the copy object code to main function

--- copyctr1.cpp
+++ copyctr1.cpp
@@ -18,5 +18,15 @@ Book(Book &other)
 	title=other.title;
 	pages=other.pages;
 }
+void display()
+{
+	cout<<"Tile:"<<title<<"\nPages:"<<pages<<endl;
+}
+int main()
+{
+	Book book1("Harry Potter",200);
+	Book book2=book1;
+	cout<<"Book1 Details:"<<endl;
+
 
 
