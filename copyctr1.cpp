a633e8f60b1622c39870d450939c62da84d4bfca
Sona243MS <sona.simmy@s.amity.edu>
2025-05-23 06:01:58 +0530

    Completed the main function by calling the display function using copy object

--- copyctr1.cpp
+++ copyctr1.cpp
@@ -27,6 +27,11 @@ int main()
 	Book book1("Harry Potter",200);
 	Book book2=book1;
 	cout<<"Book1 Details:"<<endl;
+	book1.diaply;
+	cout<<"Book 1 Details(Copy)":<<endl;
+	book2.display();
+	return 0;
+}
 
 
 
