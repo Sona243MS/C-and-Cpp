e798087d75fa4262f9453a6c43efec62156d167a
Sona243MS <sona.simmy@s.amity.edu>
2025-05-23 06:11:56 +0530

    Added the main function to the code

--- ctr2.cpp
+++ ctr2.cpp
@@ -22,4 +22,14 @@ class Wall
 		{
 			return length*height;
 		}
+		int main()
+		{
+			Wall wall1(22,20);
+			Wall wall2=wall1;
+			cout<<"Area of wall1:"<<wall1.calculateArea()<<endl;
+			cout<<"Area of wall2(Copy of wall1):"<<wall2.calculateArea()<<endl;
+			return 0;
+		}
+
+
 
