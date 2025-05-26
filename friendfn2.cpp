1888985f19b54394ee44df7bd695711e35f2e3b3
Sona243MS <sona.simmy@s.amity.edu>
2025-05-26 12:55:01 +0530

    Added the Rectangle class and declared friend function with two class objects

--- friendfn2.cpp
+++ friendfn2.cpp
@@ -2,4 +2,16 @@
 using namespace std;
 class Rectangle;
 class Circle;
+class Rectangle
+{
+private:
+    int length,width;
+public:
+    Rectangle(int l,int w)
+    {
+        length=l;
+        width=w;
+    }
+    friend void comparearea(Rectangle r,Circle c);
+};
 
