eab2096683ed096c03cec0f61c0215ec290f059a
Sona243MS <sona.simmy@s.amity.edu>
2025-05-26 12:51:16 +0530

    Added the main function

--- friendfn1.cpp
+++ friendfn1.cpp
@@ -17,4 +17,45 @@ void calculateArea(Box b1)
     int area=b1.length*b1.width;
     cout<<"Area:"<<area;
 }
+int main()
+{
+    int a,b;
+    cout<<"Enter length and width=";
+    cin>>a>>b;
+    Box b2(a,b);
+    calculateArea(b2);
+    return 0;
+}
+
+`
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+friendfn1.cpp [unix] (12:49 26/05/2025)                                                                                                                                                 20,1 All
+-- INSERT --
 `
