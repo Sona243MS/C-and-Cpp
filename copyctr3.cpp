205af74bb28d687413f7655d9ca220265779a590
Sona243MS <sona.simmy@s.amity.edu>
2025-05-26 12:37:02 +0530

    Added the default constructor to the code

--- copyctr3.cpp
+++ copyctr3.cpp
@@ -5,4 +5,15 @@ class ArrayHolder {
 private:
     int *arr;  // Should be a pointer to dynamically allocated array
     int size;
+public:
+    // Constructor (fixed)
+    ArrayHolder(int s) {  // Takes only size, not value
+        size = s;
+        arr = new int[s];  // Correctly allocate memory
+        for (int i = 0; i < s; ++i) {
+            arr[i] = i + 1;  // Initialize array elements
+        }
+        cout << "Constructor: Array at " << arr << endl;
+    }
+
 
